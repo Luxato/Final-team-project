bab0941ff758c00581b5811438069499e0427c33
Sheaxer <sheaxer@gmail.com>
2016-12-09 02:50:04 +0100

    weird shit

---
+++ matches/match-teaser.js
@@ -0,0 +1,321 @@
+var zapalky =[];
+
+var started=false;
+
+var index=0;
+
+var destroyed = 0;
+
+var left_offset=0;
+var top_offset=0;
+//var data = null;
+
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+
+function bestFit(zapalka, final)
+{
+  var eps=100;
+  var min=Number.POSITIVE_INFINITY;
+  var min_pos = null;
+  var pos=zapalka.getPosition();
+ // alert(pos.left + " " + pos.top);
+  for(var i=0; i< final.length; i++)
+  {
+    //alert("final "+ i +" left = " +final[i].left);
+    var left_low = (+final[i].left) - eps;
+    var left_high = +final[i].left +  +eps;
+    //alert(" scitam " + final[i].left + " s " + eps + " a  dostavam: " +  left_high);
+    var top_low = +final[i].top - eps;
+    var top_high = +final[i].top +  +eps;
+    var rot_low = +final[i].rot - 10;
+    var rot_high = +final[i].rot + 10;
+    //alert("testing againsts " + final[i].left + " " + final[i].top);
+    if((pos.left > left_low) && (pos.left < left_high))
+    {
+      //alert ("for " + i + " is "+ pos.left + " in " + left_low + " " + left_high);
+      if((pos.top > top_low) &&(pos.top < top_high))
+      {
+       // alert(pos.top + " in " + top_low + " " + top_high);
+        if((zapalka.getRotation() > rot_low) && (zapalka.getRotation() < rot_high))
+        {
+        //  alert("matched");
+          var dis = Math.sqrt(Math.pow((pos.left - final[i].left),2) + Math.pow((pos.top - final[i].top),2));
+          if(dis < min)
+          {
+            min = dis;
+            min_pos=i;
+          }
+        }
+        /*else
+        {
+          alert(zapalka.getRotation() + " not in " + rot_low + " " + rot_high);
+        }*/
+      }
+    }
+  }
+  return (min_pos);
+}
+
+function validate(index)
+{
+  //alert("holy fuuuuck");
+  //alert("checking");
+  var checked = [];
+  for (var i=0; i< games[index].final.length; i++)
+  {
+    checked.push(false);
+  }
+  /*for(var i = 0; i< zapalky.length; i++)
+  {
+    alert(zapalky[i].getPosition().top);
+  }*/
+  for (var i=0; i< zapalky.length; i++)
+  {
+    var f=bestFit(zapalky[i], games[index].final);
+    if(f !== null)
+    {
+      //alert("zapalka " + i + " ma miesto: " + f);
+      //alert("poz: " + zapalky[i].getPosition().left + " " + zapalky[i].getPosition().top);
+      checked[f]=true;
+    }
+  }
+  for (var i=0; i< checked.length; i++)
+  {
+    if (checked[i] == false)
+    {
+     // alert("is false");
+      return;
+    }
+  }
+ // alert(index);
+ // alert(parseInt(games[index].remove));
+  if(destroyed !== parseInt(games[index].remove))
+  {
+   // alert ("not removed enough");
+    return;
+  }
+  alert("finished");
+  finalize(games[index].final);
+  //alert(checked[2]);
+  //alert(data.games[0]s[0].final[0].left);
+}
+
+function finalize(final)
+{
+  for(var i = 0; i < final.length ; i++)
+  {
+    var pos_left = +final[i].left + 3;
+    var pos_top = +final[i].top  + 3;
+    var rot = final[i].rot;
+    zapalky[i].rotate(0);
+    //alert("setting " + pos_left + " " + pos_top);
+    zapalky[i].setPosition(pos_left,pos_top);
+    zapalky[i].rotate(rot);
+    zapalky[i].css('visibility', 'visible');
+  }
+  for(var i= final.length; i < zapalky.length; i++)
+  {
+    zapalky[i].css('visibility', 'hidden');
+  }
+}
+
+function destroy()
+{
+  $(".match").remove();
+}
+
+function setUp()
+{
+	//alert("setting up");
+	  $( ".match" ).width(160);
+    $( ".match" ).height(25);
+    $( ".match" ).draggable( 
+    {
+
+      containment: "parent", 	
+      	
+      stop: function(event, ui) 
+      {
+
+        // Show dropped position.
+        var Stoppos=$(this).getPosition();
+        var angle=$(this).getRotation();
+        validate(index);
+        console.log("STOP: \nLeft: "+ Stoppos.left + "\nTop: " + Stoppos.top);
+       // alert("Rotation: "+ angle);
+      }
+    }
+    );
+    $('<img src="./matches/match.png" width="160" height="25">').appendTo('.match');
+   
+    $('.match').click(function() {
+       var pos= $(this).getPosition();
+      // alert("left: " + pos.left + " top: "+ pos.top);	
+ 	    $(this).addRotation();
+      validate(index);
+    });
+}
+
+function load(index) {
+    destroy();
+    destroyed=0;
+    $("#desc").html ('<p class="title">' + games[index].desc + '</p>');
+    zapalky=[];
+    for (var i = 0; i < games[index].original.length; i++) {
+      var new_div = $('<div class="match"> </div>');
+      zapalky.push(new_div);
+      new_div.appendTo('#board');
+    }
+    setUp();
+    original= games[index].original;
+    final = games[index].final;
+
+
+    for(var i=0; i<zapalky.length; i++)
+    {
+      zapalky[i].setPosition(games[index].original[i].left, games[index].original[i].top);
+      var p = zapalky[i].getPosition();
+      //alert("Gotten: for " +i + " " + p.left + " when i should have gotten: " + games[index].original[i].left);
+      zapalky[i].rotate(games[index].original[i].rot);
+
+    }
+    var p = zapalky[0].getPosition();
+    left_offset = p.left - games[index].original[0].left;
+    top_offset = p.top - games[index].original[0].top;
+    
+    //alert("offset: " + left_offset + " " + top_offset);
+
+    //alert("why is this not doing")
+    for(var i=0; i<zapalky.length; i++)
+    {
+      var p = zapalky[i].getPosition();
+      //alert("Gotten: " + p.left + " when i should have gotten: " + games[index].original[i].left);
+    }
+
+}
+
+  //alert("a");
+
+  jQuery.fn.getRotation = function()
+  {
+    var matrix = $(this).css("transform");
+    if( matrix !== 'none')
+    {
+        var values = matrix.split('(')[1].split(')')[0].split(',');
+        var a = values[0];
+        var b = values[1];
+        var angle = Math.round(Math.atan2(b, a) * (180/Math.PI));
+    } else { var angle = 0; }
+
+    angle = (angle < 0) ? angle + 360 : angle;
+    return (angle);
+  }
+
+  jQuery.fn.getPosition = function()
+  {
+    var angle = $(this).getRotation();
+    $(this).rotate(0);
+    var Stoppos = $(this).position();
+    Stoppos.left -= left_offset;
+    Stoppos.top -= top_offset;
+    $(this).rotate(angle);
+    return (Stoppos);
+  }
+
+  jQuery.fn.rotate = function(angle)
+  {
+    $(this).css({'transform' : 'rotate('+ angle +'deg)'});
+  }
+
+  jQuery.fn.addRotation = function() {
+    
+    var Stoppos=$(this).getPosition();
+        //alert("STOP: \nLeft: "+ Stoppos.left + "\nTop: " + Stoppos.top);
+
+    var angle=$(this).getRotation();
+    angle +=45;
+    $(this).rotate(angle);
+  };
+
+  jQuery.fn.setPosition = function(left,top)
+  {
+    $(this).position({
+      of: $(this).parent(),
+      my: 'left+'  + left + ' top+' +  top,
+      at: 'left top'
+    });
+  }
+
+
+  $( function() {
+    
+    //$('#board').draggable();
+    //$('#1st').setPosition(200,300);
+
+    games=shuffle(data.games);
+
+    $("#startGame").click(function()
+      {
+        if(started)
+        {
+          index++;
+          if(index == games.length)
+          {
+            games=shuffle(data.games);
+            index=0;
+          }
+        }
+        started=true;
+        load(index);
+      });
+
+    $("#restart").click( function()
+      {
+        if(started)
+        {
+          load(index);
+        }
+      });
+
+    $("#destroyer").droppable(
+    {
+      accept:   '.match',
+      drop: function(event, ui)
+      {
+        destroyed++;
+        $(ui.draggable).addClass("destoyed");
+        $(ui.draggable).css("visibility", "hidden");
+        validate(index);
+      }
+    });
+
+    $("#surrender").click( function()
+    {
+      if(started)
+      {
+        finalize(games[index].final);
+      }
+    });
+    //alert(games[0].remove);
+    
+    //alert(matchData.games[0]s[0].original[0].left);
+
+  } );
