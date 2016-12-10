37dd9915c174e2cb227a31f966173e6a40fe7cae
Tomas <sheaxer@gmail.com>
2016-12-10 09:36:10 +0100

    gps suranice podla obrazka

--- maps/map2.js
+++ maps/map2.js
@@ -1,37 +1,57 @@
-var bot_left = {lat: 47.7, long: 16.84 } // lat je height
-var top_right = {lat: 49.6, long: 22.6}   // long je width
+var bot_left = {lat: 47.50393, long: 16.768537 } // lat je height
+var top_right = {lat: 49.559947, long: 22.881291}   // long je width
+
+var diff = {lat: top_right.lat - bot_left.lat, long: top_right.long - bot_left.long}
 
 
 
-
-function PairMarkerTime (marker, time)
+function marker(coordinates, left, top, gpsToPix, container, nazov)
 {
-	this.marker = marker;
-	this.time = time;
+	var fig = $('<figure class="marker_cont"> <img class="marker_show" src="./maps/index.png" width="30px" height="30px"> <aside> ' + nazov + '</aside> </figure');
+	fig.appendTo(container);
+	fig.css(
+	{
+		position: "absolute",
+		left: (coordinates.long - left.long)  * gpsToPix.long,
+		top: (top.lat - coordinates.lat )  *  gpsToPix.lat,
+		margin: 0,
+		padding: 0
+	});
 }
 
-PairMarkerTime.prototype.get = function() {
+/*PairMarkerTime.prototype.get = function() {
 	// body...
 	alert(this.marker);
-};
+};*/
 
 $( document ).ready(function()
 {
-	alert("test");
+	//alert("test");
 	var width=$("#mapa_slovenska2").width();
 	var height=$("#mapa_slovenska2").height();
 
 	//alert(width + " " + height);
 
+	//console.log(diff.lat + " " + diff.long);
 
+	var gpsToPix = {lat: (height /diff.lat ), long: (width / diff.long) }
 
-	/*$.getJSON('pamiatky.json', function (data) 
+	//console.log(gpsToPix.lat + " " + gpsToPix.long);
+
+	var markers=[];
+
+	$.getJSON('pamiatky.json', function (data) 
 	{
 
 		for (var index in data.pamiatky) {
 
+			console.log(data.pamiatky[index].sirka);
+			console.log(data.pamiatky[index].dlzka);
 
+			var coordinates = {lat: data.pamiatky[index].sirka, long: data.pamiatky[index].dlzka}
+
+			markers.push(new marker(coordinates, bot_left, top_right, gpsToPix, $('#map2_holder'), data.pamiatky[index].nazov));
 		}
 
-	});*/
+	});
 });
